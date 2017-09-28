24cd877d497c2bad16c76ba4131800a1dcf4083e
epeios-q37 <epeios-q37@users.noreply.github.com>
2017-09-28 10:01:50 +0200

    28/09/2017 10:01:50

--- src/RNCq.java
+++ src/RNCq.java
@@ -21,18 +21,18 @@
 If 'RPN' is 'false', the argument, or, if missing, 'exprALG' must be algebraic ('1 + 2').
 if 'RPN' is 'true', the argument, or, if missing, 'exprRPN' must be RPN ('1 2 +').
 */
-class Data {
+class RNCqData {
  static String exprALG = "784512.8456120 * 8456120.48651320 - 84651320.896451320 / 84651230.89645120 * 86451320.798465123 / 84651320.789465120 * 12365478985245600000000";
  static String exprRPN = "784512.8456120 8456120.48651320 * 84651320.896451320 - 84651230.89645120 / 86451320.798465123 * 84651320.789465120 / 12365478985245600000000 *";
  static boolean RPN = true;
 }
 
-class Decl {
+class JREqDecl {
  protected static String affix = "rncq";
 }
 
 // Begin of generic part.
-class JREq extends Decl {
+class JREq extends JREqDecl {
 	native public static String wrapperInfo();
 	native public static String componentInfo();
 	native private static void init( String location);
@@ -62,7 +62,7 @@ class JREq extends Decl {
   }
 
  	init( location );
-  register( "./" + prefix + Decl.affix + "jre");
+  register( "./" + prefix + JREqDecl.affix + "jre");
 /*
   try {
   System.out.println(new java.io.File(RNCqTest.class.getClassLoader().getResource(RNCqTest.class.getCanonicalName().replace('.', '/') + ".class").toURI()).getParent());
@@ -104,18 +104,18 @@ class RNCqTest {
 
   String expr;
 
-  if ( !Data.RPN ) {
+  if ( !RNCqData.RPN ) {
    if ( args.length > 0 )
     expr = args[0];
    else
-    expr = Data.exprALG;
+    expr = RNCqData.exprALG;
 
 	   System.out.println( RNCq.evaluateALG( expr ) );
   } else {
    if ( args.length > 0 )
     expr = args[0];
    else
-    expr = Data.exprRPN;
+    expr = RNCqData.exprRPN;
 
    System.out.println( RNCq.evaluateRPN( expr ) );
   }
